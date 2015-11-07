83d8f90d8a59c701274e52040a877c7dcb720d3b
Dimitri Walters <me@dimi.io>
2015-11-07 12:31:07 -0500

    Remove first sum func

--- rec.hs
+++ rec.hs
@@ -1,14 +1,10 @@
-sum' :: (Num a) => [a] -> a
-sum' [] = 0
-sum' (x:xs) = x + sum' xs
-
 dot :: (Num a) => [a] -> [a] -> a
 dot [] _ = 0
 dot _ [] = 0
 dot (x:xs) (y:ys) = x * y + dot xs ys
 
-sum'' :: (Num a) => [a] -> a
-sum'' = foldl1 (+)
+sum' :: (Num a) => [a] -> a
+sum' = foldl1 (+)
 
 fact :: Int -> Int
 fact n
