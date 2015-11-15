f1411f4dff64e9e2416a7dfa55091d384a7967b6
Dimitri Walters <me@dimi.io>
2015-11-15 16:00:26 -0500

    Use do monad block

--- monad.hs
+++ monad.hs
@@ -2,3 +2,10 @@ multiple :: (Num a, Eq a) => a -> a -> Maybe a
 multiple 0 _ = Nothing
 multiple _ 0 = Nothing
 multiple x y = Just (x * y)
+
+foo :: (Num a, Eq a) => Maybe a
+foo = do  
+    x <- multiple 3 4
+    y <- multiple x 6
+    z <- multiple y 0
+    Just (x + y)
