95c4d42a542267a05dec666bd2ff9296f61391ca
Dimitri Walters <me@dimi.io>
2015-11-26 12:21:18 -0500

    Do stuff with writer monad

--- monad.hs
+++ monad.hs
@@ -1,3 +1,5 @@
+import Control.Monad.Writer
+
 multiple :: (Num a, Eq a) => a -> a -> Maybe a
 multiple 0 _ = Nothing
 multiple _ 0 = Nothing
@@ -9,3 +11,12 @@ foo = do
     y <- multiple x 6
     z <- multiple y 0
     Just (x + y)
+
+logNumber :: Int -> Writer [String] Int  
+logNumber x = writer (x, ["Got number: " ++ show x])  
+  
+multWithLog :: Writer [String] Int  
+multWithLog = do  
+    a <- logNumber 3  
+    b <- logNumber 5  
+    return (a+b)  
